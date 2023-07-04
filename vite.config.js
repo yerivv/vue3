a5295feb534347008fc6cd7b6800584d5127328e
yeri <seo@infi.co.kr>
2023-07-04 18:13:28 +0900

    vite config 셋팅..ing

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,12 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    port: 3000,
+    open: true, //실행할때 오픈할고냐 말고냐 , 경로를 넣어도 됨. ex open: '/docs/index.html',
+  },
+  build: {
+    outDir: './dist'
   }
 })
