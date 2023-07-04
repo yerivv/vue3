fe29d18328aada8a8e9f88e0084eb4107e67a717
yeri <seo@infi.co.kr>
2023-07-04 18:55:31 +0900

    build cors error

--- vite.config.js
+++ vite.config.js
@@ -14,8 +14,9 @@ export default defineConfig({
     }
   },
   server: {
-    port: 3000,
+    port: 3000, //기본 3000에서 기본은 5173으로 변경됨
     open: true, //실행할때 오픈할고냐 말고냐 , 경로를 넣어도 됨. ex open: '/docs/index.html',
+    cors: false,
   },
   build: {
     outDir: './dist'
