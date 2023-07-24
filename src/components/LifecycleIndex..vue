ff95a77015d00c84be1d9b4a0ccaa4b60a5417d0
yeri <seo@infi.co.kr>
2023-07-24 15:37:44 +0900

    lifecycle hook index

---
+++ src/components/LifecycleIndex..vue
@@ -0,0 +1,31 @@
+<template>
+	<main>
+		<lifecycleHooks></lifecycleHooks>
+		<!-- <provideInject></provideInject> -->
+	</main>
+</template>
+
+<script>
+import provideInject from '@/components/ProvideInject.vue';
+import lifecycleHooks from '@/components/LifecycleHooks.vue';
+import { onBeforeMount, onMounted } from 'vue';
+export default {
+	components: {
+		//provideInject,
+		lifecycleHooks,
+	},
+	setup() {
+		return {};
+	},
+	// beforeCreate() {
+	// 	//vue3에선 사용X
+	// 	console.log('beforeCreate');
+	// },
+	// created() {
+	// 	//vue3에선 사용X
+	// 	console.log('created');
+	// },
+};
+</script>
+
+<style lang="scss" scoped></style>
